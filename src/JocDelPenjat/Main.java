fe69236f2e9b7acbea9528b817fdee5552fc6dab
CarlosSabauPOLITECNIC <csabau353@alumnes.politecnicllevant.cat>
2024-05-08 09:20:52 +0200

    Classes fusionades en una sola (se prioritza funcionalitat abans que refactorització)

--- src/JocDelPenjat/Main.java
+++ src/JocDelPenjat/Main.java
@@ -1,14 +1,56 @@
 package JocDelPenjat;
 
-import java.awt.*;
 import java.util.*;
 import java.util.Random;
-import java.util.random.RandomGenerator;
+
 public class Main {
 
+
+    // ----- Todos los valores ----- //
+   static int posParaulaElegida;
+   Scanner sc = new Scanner(System.in);
+   static String paraulaRandom;
+   static Random random = new Random();
+   static ArrayList<String> paraulesPerAdivinar = new ArrayList<>();
+
+    // ----------------------------- //
     public static void main(String[] args) {
-        joc.elegirParaulaAleatoria();
-        joc.partida();
-      
+        partida();
+
+
+
+    }
+
+
+    public static void elegirParaulaAleatoria() {
+
+        paraulesPerAdivinar.add("Wisdom");
+        paraulesPerAdivinar.add("Carlos");
+        paraulesPerAdivinar.add("Patinete");
+        paraulesPerAdivinar.add("Paella");
+        posParaulaElegida = random.nextInt(paraulesPerAdivinar.size());
+        paraulaRandom = paraulesPerAdivinar.get(posParaulaElegida);
+    }
+    public static String generadorParaulaEncriptada(){
+
+        for (int i = 0; i < paraulaRandom.length(); i++) {
+            System.out.print("_ ");
+
+        }
+        return paraulaRandom;
+    }
+    public static void partida() {
+        System.out.println("Benvingut al joc del penjat.");
+        System.out.println("------------------------");
+        System.out.print("Adivina la paraula:  "); elegirParaulaAleatoria(); generadorParaulaEncriptada();
+        System.out.print("Lletra: ");
+
+        posParaulaElegida = random.nextInt(paraulesPerAdivinar.size());
+        paraulaRandom = paraulesPerAdivinar.get(posParaulaElegida);
+
     }
 }
+/*char lletra = sc.next().charAt(0);
+        for (int i = 0; i < paraulaRandom.length() ; i++) {
+
+        }*/
